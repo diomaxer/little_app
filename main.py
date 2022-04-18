7231dd3a2161931b4bde4b935eabc969be22cf52
diomaxer <diomaxer@gmail.com>
2022-04-18 23:47:48 +0300

    api product/

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+@app.get("product/")
+async def root():
+    return {
+        "eggs": 12,
+        "spam": 10,
+    }
