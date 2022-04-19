48f44228d63f86c53b59b8da2a32ce6db1b46075
diomaxer <diomaxer@gmail.com>
2022-04-19 19:24:25 +0300

    .env

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip3 install -r requirements.txt
 
 COPY . /app
 
-CMD uvicorn main:app --host 0.0.0.0 --port 5000
+CMD bash docker.sh
--- database/alchemy_db.py
+++ database/alchemy_db.py
@@ -1,9 +1,14 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
+host = os.getenv('DB_HOST')
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/fast_api_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_DATABASE')}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
